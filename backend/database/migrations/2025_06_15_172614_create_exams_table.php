02a80f5dece9578239f9558ffce22d3db2a94cb5
omartins-zs <gabrielmatheus209@gmail.com>
2025-06-16 15:57:08 -0300

    Criando a Migration de Exames

---
+++ backend/database/migrations/2025_06_15_172614_create_exams_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('exams', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->enum('laterality', ['OD', 'OE', 'AO'])->nullable();
+            $table->string('comment');
+            $table->enum('group', ['Individual', 'Grupo 1', 'Grupo 2', 'Grupo 3', 'Grupo 4', 'Grupo 5']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('exams');
+    }
+};
