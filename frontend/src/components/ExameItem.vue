7fc94f7eca3be50d645d6534047e62b75c2a8c5d
omartins-zs <gabrielmatheus209@gmail.com>
2025-06-16 00:06:09 -0300

    Criando o Componente de Item do Exame

---
+++ frontend/src/components/ExameItem.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-list-item>
+    <v-list-item-content>
+      <v-list-item-title>{{ exam.name }} ({{ exam.group }})</v-list-item-title>
+      <v-list-item-subtitle>{{ exam.comment }}</v-list-item-subtitle>
+    </v-list-item-content>
+  </v-list-item>
+</template>
+
+<script>
+export default { name: 'ExameItem', props: { exam: Object } }
+</script>
